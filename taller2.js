944a3f800d4c8189383ef423cccf4d170d4d706d
Lena Castillo <lenac@uninorte.edu.co>
2025-02-21 20:19:16 -0500

    declaracion

--- taller2.js
+++ taller2.js
@@ -29,7 +29,8 @@ function sum(array) {
 function missingNumbers(numeros) {
     if (numeros.length === 0) return []
   
-    let min = numeros[0], max = numeros[0];
+    let min = numeros[0]
+    let max = numeros[0]
     for (let i = 1; i < numeros.length; i++) {
       if (numeros[i] < min) min = numeros[i];
       if (numeros[i] > max) max = numeros[i];
